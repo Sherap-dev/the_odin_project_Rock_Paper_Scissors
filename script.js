2046e2cdd765adfbca9711e5895ddc20f59ef517
sherap <3dsherap@gmail.com>
2025-10-31 12:25:31 +0530

    add scores and five round game function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 
 function randomMove(){
     let computerMove = ''
@@ -20,7 +23,10 @@ function playerMove(move){
     if (move === 'rock' || move === 'paper' || move === 'scissors'){
         console.log(`you chose ${move}`)
         return move ;
-    } else console.log('please enter a valid move')
+    } else {
+        console.log('please enter a valid move');
+        return null;
+    }
 }
 
 function playRound(player , computer){
@@ -30,17 +36,49 @@ function playRound(player , computer){
     computer === 'paper' && player === 'rock' ||
     computer === 'scissors' && player === 'paper'){
         console.log('you lost')
+        computerScore ++
     }else if (computer === player){
         console.log ('its a tie')
     }
-    else console.log('you win')
+    else {
+        console.log('you win');
+        humanScore ++
 
+    } 
+
+    console.log(`Score → You: ${humanScore} | Computer: ${computerScore}`);
 }
 
-const player = playerMove(prompt('choose'))
-const computer = randomMove()
 
-playRound(player,computer)
+
+
+function playGame(){
+    for (i =1; i<=5; i++){
+        console.log(`round ${i}`)
+
+
+    const player = playerMove(prompt('choose'))
+    if(player){
+        const computer = randomMove();
+        playRound(player,computer);
+    }
+    }
+
+    if (humanScore > computerScore){
+        console.log('you won the game');
+    }else if (humanScore < computerScore){
+        console.log('you lost the game')
+    }else console.log('the game was a tie')
+}
+
+playGame()
+
+
+
+
+
+
+
 
 
 
